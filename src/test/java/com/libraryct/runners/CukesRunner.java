6442d520af14d292e1129de6703990dacb78c5a9
Drita Alla <dritaalla3@gmail.com>
2020-10-28 15:39:10 +0100

    hope it works

---
+++ src/test/java/com/libraryct/runners/CukesRunner.java
@@ -0,0 +1,4 @@
+package com.libraryct.runners;
+
+public class CukesRunner {
+}
---
+++ src/test/java/com/libraryct/stepDefinitions/LoginStepDefs.java
@@ -0,0 +1,7 @@
+package com.libraryct.stepDefinitions;
+
+public class LoginStepDefs {
+    public static void main(String[] args) {
+        System.out.println("why hard times");
+    }
+}

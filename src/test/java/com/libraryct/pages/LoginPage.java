d858d96114c67838992506b940e078a630bd2728
Selma-Atabas <selma-aykal43@hotmail.com>
2020-11-13 00:35:24 +0100

    accesmodule added

--- src/test/java/com/libraryct/pages/LoginPage.java
+++ src/test/java/com/libraryct/pages/LoginPage.java
@@ -1,10 +1,14 @@
 package com.libraryct.pages;
 
+import com.libraryct.utilities.BrowserUtils;
 import com.libraryct.utilities.ConfigurationReader;
 import com.libraryct.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class LoginPage {
     public LoginPage(){
@@ -20,11 +24,28 @@ public class LoginPage {
     @FindBy(css ="[type='submit']")
     public WebElement signinBtn;
 
-    @FindBy(linkText = "Books")
+    @FindBy(xpath = "//*[@id=\"menu_item\"]/li[3]/a/span[1]")
     public WebElement books;
 
     @FindBy(linkText = "Borrowing Books")
     public WebElement borrowingBooks;
+
+    @FindBy(xpath = "//*[@id=\"menu_item\"]/li[2]/a/span[1]")
+    public WebElement Users;
+
+    @FindBy(xpath = "//*[@id=\"menu_item\"]/li[1]/a/span[1]")
+    public WebElement Dashboard;
+
+    @FindBy(xpath = "//*[@id=\"books\"]/div[1]/div[1]/h3")
+    public WebElement BookManagement;
+
+    @FindBy(xpath = "//*[@id=\"users\"]/div[1]/div[1]/h3")
+    public WebElement UserManagement;
+
+    @FindBy(xpath = "//*[@id=\"dashboard\"]/div/div/div[1]/div/div/div[2]/h6")
+    public WebElement usersindashboard;
+
+
 
 
 
@@ -41,6 +62,21 @@ public class LoginPage {
         signinBtn.click();
     }
 
+    public void accessmodule(String module) {
 
+        if (module.equalsIgnoreCase("books")) {
+            books.click();
+            BrowserUtils.waitFor(2);
+            Assert.assertTrue(BookManagement.isDisplayed());
+        } else if (module.equalsIgnoreCase("Users")) {
+            Users.click();
+            BrowserUtils.waitFor(4);
+            Assert.assertTrue(UserManagement.isDisplayed());
+        } else if (module.equalsIgnoreCase("Dashboard")) {
+            Dashboard.click();
+            BrowserUtils.waitFor(2);
+            Assert.assertTrue(usersindashboard.isDisplayed());
+        }
+    }
 
 }
--- src/test/java/com/libraryct/stepDefinitions/modulesStepDef.java
+++ src/test/java/com/libraryct/stepDefinitions/modulesStepDef.java
@@ -7,6 +7,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class modulesStepDef {
 
     @When("user login as a student")
@@ -27,6 +29,19 @@ public class modulesStepDef {
     @Then("user see Borrowing Books")
     public void user_see_Borrowing_Books() {
        Assert.assertTrue(new LoginPage().borrowingBooks.isDisplayed());
+        BrowserUtils.waitFor(2);
+    }
+    @When("user login as a librarian")
+    public void user_login_as_a_librarian() {
+        new LoginPage().loginAsLibrarian();
+        BrowserUtils.waitFor(7);
+    }
+
+    @Then("user access following  {string} module")
+    public void user_access_following_module(String options) {
+
+
+        new LoginPage().accessmodule(options);
         BrowserUtils.waitFor(2);
     }
 
